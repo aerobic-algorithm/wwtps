c59b2823565f781a206aaa686431c423f7b6d281
K1rubel <kiruabiyu@gmail.com>
2026-06-05 05:10:12 +0300

    fontawesome icons for whatsapp and telegram

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { FaTelegramPlane, FaWhatsapp } from 'react-icons/fa'
 import { HiOutlineClipboardDocument, HiOutlineEnvelope, HiOutlinePhone } from 'react-icons/hi2'
 
 const FORM_ENDPOINT = 'https://formspree.io/f/your-form-id'
@@ -66,9 +67,7 @@ export default function Contact() {
           className="channel-card whatsapp"
         >
           <div className="channel-icon">
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="currentColor">
-              <path d="M16 0C7.2 0 0 7.2 0 16c0 2.8.8 5.6 2.2 8l-2.2 6.4L8 26.8c2.4 1.4 5.2 2.2 8 2.2 8.8 0 16-7.2 16-16S24.8 0 16 0zm0 29c-2.6 0-5.2-.6-7.4-1.8l-.6-.3L3 28l.8-2.3-.2-.6C2.2 21.2 1.6 18.6 1.6 16c0-7.8 6.4-14.2 14.4-14.2s14.2 6.4 14.2 14.2-6.4 14.2-14.2 14.2zm7.8-10.6c-.4-.2-2.6-1.3-3-1.4-.4-.2-.8-.2-1.2.2-.4.4-1.4 1.4-1.8 1.8-.4.4-.8.4-1.2.2-.4-.2-1.6-.6-3.2-2-1.2-1-2-2.4-2.2-2.8-.2-.4 0-.8.2-1 .2-.2.4-.6.6-.8.2-.2.2-.4.4-.8.2-.4.2-.8 0-1.2-.2-.4-1.2-2.8-1.6-3.8-.4-1-.8-.8-1.2-.8h-1c-.4 0-1 .2-1.6.8-1.4 1.4-1.8 3.2-1.8 5 0 1.8.8 4.2 2.2 6.2 2.8 4.2 7.2 5.8 10 5.8 1 0 2-.2 2.8-.6 1-.4 1.8-.8 2-.8.6 0 .8-.4.8-.8 0-1.6-1-3.2-1.4-3.4z" />
-            </svg>
+            <FaWhatsapp size={28} />
           </div>
           <h3>WhatsApp</h3>
           <p>Chat with us on WhatsApp for quick responses</p>
@@ -81,9 +80,7 @@ export default function Contact() {
           className="channel-card telegram"
         >
           <div className="channel-icon">
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="currentColor">
-              <path d="M16 0C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16-7.2-16-16-16zm0 28c-6.6 0-12-5.4-12-12s5.4-12 12-12 12 5.4 12 12-5.4 12-12 12zm6.6-13.4l-9.2-3.6c-.8-.4-1.4-.2-1.4.8v2.2l-5 2.4c-.6.2-.6.6 0 .8l5 2.4v2.2c0 1 .6 1.2 1.4.8l9.2-3.6c.8-.4.8-1 0-1.4z" />
-            </svg>
+            <FaTelegramPlane size={28} />
           </div>
           <h3>Telegram</h3>
           <p>Reach us on Telegram for instant messaging</p>
