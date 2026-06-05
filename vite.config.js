97426176c161ef1ba215d72d851a96e4b3a6969b
K1rubel <kiruabiyu@gmail.com>
2026-06-05 06:24:06 +0300

    edited base repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wwtps/',
+  base: process.env.NODE_ENV === 'production' ? '/wwtps/' : '/',
   plugins: [react()],
 })
